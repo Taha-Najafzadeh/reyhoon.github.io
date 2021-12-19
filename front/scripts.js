0da78634a0812d5e234931da29dfe2b21ca012d9
Taha <59098390+Taha-Najafzadeh@users.noreply.github.com>
2021-12-19 20:50:50 +0330

    added files

---
+++ front/scripts.js
@@ -0,0 +1,15 @@
+$('#Email').focus(function () {
+    $('#EmailIcon').addClass('SvGFocusColor');
+    }).blur(function () 
+    {
+    $('#EmailIcon').removeClass('SvGFocusColor');
+
+});
+
+$('#Password').focus(function () {
+    $('#PasswordIcon').addClass('SvGFocusColor');
+    }).blur(function () 
+    {
+    $('#PasswordIcon').removeClass('SvGFocusColor');
+
+});
