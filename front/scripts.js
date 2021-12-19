ffc374a4785a6e60035981d50ecdb7f9cc809c9c
mehdi <mahdydarvish@gmail.com>
2021-12-19 23:23:44 +0330

    added sign-up page

--- front/scripts.js
+++ front/scripts.js
@@ -6,10 +6,25 @@ $('#Email').focus(function () {
 
 });
 
+$('#Person').focus(function () {
+    $('#PersonIcon').addClass('SvGFocusColor');
+    }).blur(function () 
+    {
+    $('#PersonIcon').removeClass('SvGFocusColor');
+
+});
 $('#Password').focus(function () {
     $('#PasswordIcon').addClass('SvGFocusColor');
     }).blur(function () 
     {
     $('#PasswordIcon').removeClass('SvGFocusColor');
+
+});
+
+$('#Password2').focus(function () {
+    $('#PasswordIcon2').addClass('SvGFocusColor');
+    }).blur(function () 
+    {
+    $('#PasswordIcon2').removeClass('SvGFocusColor');
 
 });
