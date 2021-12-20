e280c2ea70e8b0abf4287bb37265fd883649fd00
mehdi <mahdydarvish@gmail.com>
2021-12-20 12:46:42 +0330

    combined signup and login pages

--- front/scripts.js
+++ front/scripts.js
@@ -6,13 +6,7 @@ $('#Email').focus(function () {
 
 });
 
-$('#Person').focus(function () {
-    $('#PersonIcon').addClass('SvGFocusColor');
-    }).blur(function () 
-    {
-    $('#PersonIcon').removeClass('SvGFocusColor');
 
-});
 $('#Password').focus(function () {
     $('#PasswordIcon').addClass('SvGFocusColor');
     }).blur(function () 
@@ -21,10 +15,51 @@ $('#Password').focus(function () {
 
 });
 
+
+$('#Person').focus(function () {
+    $('#PersonIcon').addClass('SvGFocusColor');
+    }).blur(function () 
+    {
+    $('#PersonIcon').removeClass('SvGFocusColor');
+});
+
+$('#Email_up').focus(function () {
+    $('#EmailIcon_up').addClass('SvGFocusColor');
+    }).blur(function () 
+    {
+    $('#EmailIcon_up').removeClass('SvGFocusColor');
+
+});
+
+$('#Password_up').focus(function () {
+    $('#PasswordIcon_up').addClass('SvGFocusColor');
+    }).blur(function () 
+    {
+    $('#PasswordIcon_up').removeClass('SvGFocusColor');
+
+});
 $('#Password2').focus(function () {
     $('#PasswordIcon2').addClass('SvGFocusColor');
     }).blur(function () 
     {
     $('#PasswordIcon2').removeClass('SvGFocusColor');
 
+});
+
+$( document ).ready( function() {
+  $( "#signin" ).click( function() {
+    $( "#form2" ).hide( 100 );
+    $( "#form1" ).show( 100 );
+    $("#signin").addClass("active");
+    $("#signup").removeClass("active");
+  });
+});
+
+$( document ).ready( function() {
+  $( "#signup" ).click( function() {
+    $( "#form1" ).hide( 100 );
+    $( "#form2" ).show( 100 );
+    $("#signup").addClass("active");
+    $("#signin").removeClass("active");
+  });
 });
